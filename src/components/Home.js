cb1bacaaafa5b1fc8d347c301c0933d2efd5b14e
kevinmsavsani <kevinmsavsani@gmail.com>
2020-07-07 16:10:26 +0530

    category added

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,12 +9,54 @@ import { Actions } from 'react-native-router-flux';
 import { Container, Content, View, Header, Icon, Button, Left, Right, Body, Title, List, ListItem, Thumbnail, Grid, Col } from 'native-base';
 
 import Navbar from '../screens/Navbar';
+import CategoryBlock from '../screens/CategoryBlock';
 
-const Home = () => {
-    var left = (<Left><Button transparent><Icon name='menu' /></Button></Left>);
-    var right = (<Right><Button transparent><Icon name='menu' /></Button></Right>);
-    return (
-        <Navbar left={left} right={right} title="My Navbar" />
-    )
+export default class Home extends React.Component {
+    render() {
+        var left = (<Left><Button transparent><Icon name='menu' /></Button></Left>);
+        var right = (<Right><Button transparent><Icon name='menu' /></Button></Right>);
+        return (
+            <Container>
+                <Navbar left={left} right={right} title="MY STORE" />
+                <Content>
+                    {this.renderCategories()}
+                </Content>
+            </Container>
+        )
+    }
+
+    renderCategories() {
+        let cat = [];
+        for(var i=0; i<categories.length; i++) {
+          cat.push(
+            <CategoryBlock key={categories[i].id} id={categories[i].id} image={categories[i].image} title={categories[i].title} />
+          );
+        }
+        return cat;
+    }
+
 }
-export default Home
+
+
+var categories = [
+  {
+    id: 1,
+    title: 'MEN',
+    image: 'http://res.cloudinary.com/atf19/image/upload/c_scale,w_489/v1500284127/pexels-photo-497848_yenhuf.jpg'
+  },
+  {
+    id: 2,
+    title: 'WOMEN',
+    image: 'http://res.cloudinary.com/atf19/image/upload/c_scale,w_460/v1500284237/pexels-photo-324030_wakzz4.jpg'
+  },
+  {
+    id: 3,
+    title: 'KIDS',
+    image: 'http://res.cloudinary.com/atf19/image/upload/c_scale,w_445/v1500284286/child-childrens-baby-children-s_shcevh.jpg'
+  },
+  {
+    id: 4,
+    title: 'ACCESORIES',
+    image: 'http://res.cloudinary.com/atf19/image/upload/c_scale,w_467/v1500284346/pexels-photo-293229_qxnjtd.jpg'
+  }
+];
