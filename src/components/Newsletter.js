400557ca96fe60af212767c371239d58ccd294aa
kevinmsavsani <kevinmsavsani@gmail.com>
2020-07-07 21:57:14 +0530

    contact, newsletter, wishlist added

--- src/components/Newsletter.js
+++ src/components/Newsletter.js
@@ -1,18 +1,52 @@
 /**
-* This is the Home page
+* This is the newsletter page
 **/
 
 // React native and others libraries imports
-import React from 'react'
-import { TouchableOpacity, Text } from 'react-native';
+import React, { Component } from 'react';
+import { Container, View, Icon, Left, Button, Item, Input } from 'native-base';
 import { Actions } from 'react-native-router-flux';
 
+// Our custom files and classes import
+import Text from '../screens/Text';
+import Navbar from '../screens/Navbar';
+import Colors from '../Colors';
 
-const NewsLetter = () => {
-   return (
-      <TouchableOpacity style = {{ margin: 128 }}>
-         <Text>This is NewsLetter!</Text>
-      </TouchableOpacity>
-   )
+export default class Newsletter extends Component {
+  constructor(props) {
+      super(props);
+      this.state = {
+        email: ''
+      }
+  }
+
+  render() {
+    var left = (
+      <Left style={{flex:1}}>
+        <Button transparent onPress={() => Actions.pop()}>
+          <Icon name="ios-close" size={38} style={{fontSize: 38}} />
+        </Button>
+      </Left>
+    );
+    return(
+      <Container style={{backgroundColor: '#fdfdfd'}}>
+          <Navbar left={left} title="NEWSLETTER" />
+            <View style={{flex: 1, justifyContent: 'center', alignItems: 'center', paddingLeft: 50, paddingRight: 50}}>
+              <Item>
+                  <Icon active name='ios-mail' />
+                  <Input placeholder='Your email address' onChangeText={(text) => this.setState({email: text})}/>
+              </Item>
+              <View style={{alignItems: 'center'}}>
+                <Button onPress={() => this.subscribe()} style={{backgroundColor: Colors.navbarBackgroundColor, marginTop: 20}}>
+                  <Text style={{color: '#fdfdfd'}}>Subscribe</Text>
+                </Button>
+              </View>
+            </View>
+      </Container>
+    );
+  }
+
+  subscribe() {
+    alert('Subscribe address: '+this.state.email);
+  }
 }
-export default NewsLetter
