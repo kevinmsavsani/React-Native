f3ab1502813d51f0e3adebe87919abbbacc92e13
kevinmsavsani <kevinmsavsani@gmail.com>
2020-07-07 15:44:44 +0530

    Navbar added

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,13 +6,15 @@
 import React from 'react'
 import { TouchableOpacity, Text } from 'react-native';
 import { Actions } from 'react-native-router-flux';
+import { Container, Content, View, Header, Icon, Button, Left, Right, Body, Title, List, ListItem, Thumbnail, Grid, Col } from 'native-base';
 
+import Navbar from '../screens/Navbar';
 
 const Home = () => {
-   return (
-      <TouchableOpacity style = {{ margin: 128 }}>
-         <Text>This is HOME!</Text>
-      </TouchableOpacity>
-   )
+    var left = (<Left><Button transparent><Icon name='menu' /></Button></Left>);
+    var right = (<Right><Button transparent><Icon name='menu' /></Button></Right>);
+    return (
+        <Navbar left={left} right={right} title="My Navbar" />
+    )
 }
 export default Home
